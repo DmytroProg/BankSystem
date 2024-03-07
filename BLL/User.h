1c79855b2d0f5b3d6d79a9d2ac07ee849c4ebede
DmytroProg <dmytroprog@gmail.com>
2024-03-07 23:14:47 +0200

    Added User model

--- BLL/User.h
+++ BLL/User.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
 class User
 {
+public:
+	User(){}
+	User(string name, string login, string password) 
+		: name(name), login(login), password(password){}
+
+	string getName() const { return name; }
+	string getLogin() const { return login; }
+	string getPassword() const { return password; }
+
+	void setName(string name) { this->name = name; }
+	void setLogin(string login) { this->login = login; }
+	void setPassword(string password) { this->password = password; }
+
+private:
+	string name;
+	string login;
+	string password;
 };
 
