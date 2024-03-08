e397b7cb678a3433597073da6bed3900885ed965
DmytroProg <dmytroprog@gmail.com>
2024-03-08 20:03:44 +0200

    Added UI App component

---
+++ BankSystem/App.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+#include "../BLL/ServiceBase.h";
+#include "../BLL/UserService.h";
+#include "../BLL/User.h";
+using namespace std;
+
+class App
+{
+public:
+	App()
+	{
+		userService = new UserService();
+		User user("John", "login", "1234");
+		userService->add(user);
+	}
+
+	void run() {
+		int index = menuLogin();
+
+		if (index == 1) {
+			cout << "Enter login: ";
+			string login;
+			cin >> login;
+			auto users = userService->getAll();
+			auto user = find_if(users.begin(), users.end(), 
+				[login](User u) { return u.getLogin() == login; });
+
+			cout << (*user).getName() << endl;
+		}
+	}
+
+private:
+	ServiceBase<User>* userService;
+
+	int menuLogin() {
+		cout << "1. Login" << endl;
+		cout << "2. Register" << endl;
+
+		int n;
+		cin >> n;
+		return n;
+	}
+};
+
--- BankSystem/BankSystem.cpp
+++ BankSystem/BankSystem.cpp
@@ -1,17 +1,7 @@
-#include <iostream>
-#include "../BLL/UserService.h"
+#include "App.h"
 
 int main()
 {
-    UserService userService;
-
-    User user("John", "user@gmail.com", "qwerty");
-    User user1("Alice", "alice@gmail.com", "qwerty");
-    userService.add(user);
-    userService.update(user, user1);
-    //userService.remove(user);
-
-    for (auto u : userService.getAll()) {
-        cout << u.getName() << endl;
-    }
+	App app;
+	app.run();
 }
